0b185c8a5a0ffeefa6a79202910a7a65e1b27d9c
rjcorflo <rj.corchero@gmail.com>
2017-04-22 01:22:06 +0200

    [DEV] Add Greeter command

---
+++ src/Command/GreetCommand.php
@@ -0,0 +1,59 @@
+<?php
+namespace RJ\Command;
+
+use RJ\Greeter;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class GreetCommand extends Command
+{
+    /**
+     * @var Greeter $greeter
+     */
+    protected $greeter;
+
+    /**
+     * Constructor
+     *
+     * @param Greeter $greeter
+     */
+    public function __construct(Greeter $greeter)
+    {
+        parent::__construct();
+        $this->greeter = $greeter;
+    }
+
+    protected function configure()
+    {
+        $this->setName('greet')
+            ->setDescription('Greet to someone')
+            ->setHelp('Launch a greet to introduced name.')
+            ->addArgument('name', InputArgument::REQUIRED,
+                'Name of person to be greeted')
+            ->addOption('yell', 'y', InputOption::VALUE_NONE);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $name = $input->getArgument('name');
+        $yell = $input->getOption('yell');
+
+        $greetNameString = $this->greeter->greet($name, $yell);
+
+        $io->text($greetNameString);
+
+        $numberOfTimesGreeted = $this->greeter->countGreetings($name);
+        if (1 === $numberOfTimesGreeted) {
+            $io->text('(First time!)');
+        } else {
+            $io->text(sprintf('(%d times)', $numberOfTimesGreeted));
+        }
+    }
+
+}
