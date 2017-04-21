bc18a29e679fabf4b4033114f125d611d74fa7a0
rjcorflo <rj.corchero@gmail.com>
2017-04-22 00:46:54 +0200

    [DEV] Add Greeter service

---
+++ src/Greeter.php
@@ -0,0 +1,55 @@
+<?php
+namespace RJ;
+
+use RJ\Repository\GreetDataInterface;
+
+class Greeter
+{
+    /**
+     * @var \RJ\Repository\GreetDataInterface Repository to access greeter data
+     */
+    protected $greeterRepository;
+
+    /**
+     * Greeter constructor.
+     *
+     * @param \RJ\Repository\GreetDataInterface $greeterRepository
+     */
+    public function __construct(GreetDataInterface $greeterRepository)
+    {
+        $this->greeterRepository = $greeterRepository;
+    }
+
+    /**
+     * Builds the greeting for someone (you can yell on it if you want!)
+     *
+     * @param  string $name
+     * @param  bool $yell wanna yell?
+     *
+     * @return string
+     */
+    public function greet(string $name, bool $yell = false)
+    {
+        $output = sprintf('Hello %s', $name);
+
+        if ($yell) {
+            $output = strtoupper($output);
+        }
+
+        $this->greeterRepository->incrementNumberOfGreetings($name);
+
+        return $output;
+    }
+
+    /**
+     * Will tell you how many times you greet someone.
+     *
+     * @param  string $name
+     *
+     * @return int
+     */
+    public function countGreetings(string $name) : int
+    {
+        return $this->greeterRepository->getNumberOfGreetings($name);
+    }
+}
